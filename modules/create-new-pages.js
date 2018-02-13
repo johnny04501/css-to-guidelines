37e0b17f2d4e019040b048f0082f930577a608dd
Johnny <johnny04501@azet.sk>
2018-02-13 17:01:13 +0100

    update config script has a new functions

--- modules/create-new-pages.js
+++ modules/create-new-pages.js
@@ -3,23 +3,34 @@ var prompt      = require('gulp-prompt');
 var fs          = require('fs'); 
 var rename 	    = require('gulp-rename');
 
-var getConfig = JSON.parse(fs.readFileSync('./config.json')); 
+var getConfig;
 var preparePush;
-var output;
+var output; 
+if (fs.existsSync('./config.json')) {
+	var getFile = fs.statSync('./config.json');
+	var fileSize = getFile.size;
+} 
 
 gulp.task('update-config', function(){
-  return gulp.src(['./config.json'])
-    .pipe(prompt.prompt({
-        type: 'input',
-        name: 'task',
-        message: 'Name for your page:'
-    }, function(res){
-    	// prepare object for push to config json file
-        var preparePush = {
-			"pageName" : res.task,
-			"hasChild" : false,
-			"child"	   : []
-		};
+	if(fileSize == 0){
+		var getConfig = fs.readFileSync('./config.json');
+	}
+	else{		
+		var getConfig = JSON.parse(fs.readFileSync('./config.json'));
+	} 
+	console.log(getConfig);
+  	return gulp.src(['./config.json'])
+	    .pipe(prompt.prompt({
+	        type: 'input',
+	        name: 'task',
+	        message: 'Name for your page:'
+	    }, function(res){
+	    	// prepare object for push to config json file
+	        var preparePush = {
+				"pageName" : res.task,
+				"hasChild" : false,
+				"child": []
+  			} 
 		// push to config json
         getConfig.push(preparePush); 
         // stringify before final write to config json 
